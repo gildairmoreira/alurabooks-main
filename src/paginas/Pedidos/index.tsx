7fbff35c492b0422071f2ef8334ee24b647cf303
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-09-18 23:15:56 -0300

    Update index.tsx

--- src/paginas/Pedidos/index.tsx
+++ src/paginas/Pedidos/index.tsx
@@ -22,6 +22,20 @@ const Pedidos = () => {
         .catch(erro => console.log(erro))
     }, [])
 
+    const excluir = (pedido: IPedido) =>
+    {
+        const token = sessionStorage.getItem('token')
+        axios.delete('http://localhost:8000/pedidos/' + pedido.id, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).then(() =>
+        {
+            setPedidos(pedidos.filter(p => p.id !== pedido.id))
+        })
+            .catch(erro => console.log(erro))
+    }
+
     return (<section className="pedidos">
         <h1>Meus pedidos</h1>
         {pedidos.map(pedido => (<div className="pedido" key={pedido.id}>
@@ -30,6 +44,9 @@ const Pedidos = () => {
                 <li>Data do pedido: <strong>{new Date(pedido.data).toLocaleDateString()}</strong></li>
                 <li>Valor total: <strong>{formatador.format(pedido.total)}</strong></li>
                 <li>Entrega realizada em: <strong>{new Date(pedido.entrega).toLocaleDateString()}</strong></li>
+                <li>
+                    <button onClick={() => excluir(pedido)}>Excluir</button>
+                </li>
             </ul>
             <AbBotao texto="Detalhes"/>
         </div>))}
