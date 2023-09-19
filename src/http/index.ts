1bd90e1c442f216f2729afe5e4d5e9335c36fc68
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-09-19 20:02:33 -0300

    auth

--- src/http/index.ts
+++ src/http/index.ts
@@ -8,5 +8,21 @@ const http = axios.create({
   }
 })
 
+http.interceptors.request.use(function (config)
+{
+  // Do something before request is sent (ou "fazer alguma coisa com o erro da requisição")
+  const token = sessionStorage.getItem('token')
+  if (token && config.headers)
+  {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config;
+}, function (error)
+{
+  // Do something with request error (ou "fazer alguma coisa com o erro da requisição")
+  console.log('erro no interceptor do axios')
+  return Promise.reject(error);
+});
+
 export default http
 
