7e7c37ffc92fa7f8aa0529cc31e3d69bcf41b6ca
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-09-20 10:58:39 -0300

    teste nova branch

---
+++ src/componentes/TesteQuerryBranch/index.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react';
+
+interface TesteQuerryBranchProps {
+  children: ReactNode;
+}
+
+function TesteQuerryBranch({ children }: TesteQuerryBranchProps) {
+  return (
+    <>
+      <h1>TesteQuerryBranch</h1>
+      {children}
+    </>
+  );
+}
+
+export default TesteQuerryBranch;
